c39a80f7705c9b66dc9b461850ea7253e26104ee
Dan Kozlowski <dan@surfo.us>
2011-08-29 09:53:43 -0700

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Edge::Application.config.secret_token = '952069b165747468a8902a291c9baed5f76f32e170798178d7723d54fc542e911e9856e0de11ebed58c59b54d9b28c51d7fd155e7e6bd9d44f0643627d238a17'
