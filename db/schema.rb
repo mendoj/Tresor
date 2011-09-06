d4b364e4b5e1d6199ef48cbdb4981e2c51453444
Dan Kozlowski <dan@surfo.us>
2011-09-06 12:49:07 -0700

    raked.

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110829193248) do
+ActiveRecord::Schema.define(:version => 20110906194658) do
 
   create_table "contents", :force => true do |t|
     t.string   "title"
     t.text     "body"
     t.string   "context"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "designers", :force => true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.text     "bio"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
